f504156b8f68af4942393caae8db0e5fb31fd30b
heidegomes <heidegc@hotmail.com>
2023-06-22 19:13:32 -0300

    atualizando branch

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,43 +1,67 @@
-// Desafio 11
-function generatePhoneNumber(numeros) {
-  // seu código aqui
-  // verificar se o array tem 11 numeros
-  if (numeros.lenght != 11) {
-    return 'Array com tamanho incorreto';
-  }
-  // verificar se os numeros são menor que 0 ou maior que 9
-  for (let i = 0; i < numeros.length; i++) {
-  if (numeros[i] < 0 || numeros[i] > 9) {
-    return 'não é possível gerar um número de telefone com esses valores';
-  }
-}
+// // Desafio 11
+// function generatePhoneNumber(numeros) {
+//   // seu código aqui
+//   // verificar se o array tem 11 numeros
+//   if (numeros.lenght != 11) {
+//     return 'Array com tamanho incorreto';
+//   }
+//   // verificar se os numeros são menor que 0 ou maior que 9
+//   for (let i = 0; i < numeros.length; i++) {
+//     if (numeros[i] < 0 || numeros[i] > 9) {
+//       return 'não é possível gerar um número de telefone com esses valores';
+//     }
+//   }
 
-//verificar se um numero se repete 3 vezes ou mais
-let maiorNumero = numeros[0];
-let countMaiorNumero = 1;
-for (let i = 1; i < numeros.length; i += 1) {
-  let numeroAtual = numeros[i];
-  if (numeroAtual > maiorNumero) {
-    maiorNumero = numeroAtual;
-    countMaiorNumero = 1;
-  } else if (numeroAtual === maiorNumero) {
-    countMaiorNumero += 1;
-  }
+  //verificar se um numero se repete 3 vezes ou mais
+  function maisRepetido(numeros) {
+    let contRepetido = 0;
+    let contNumero = 0;
+    let indexNumeroRepetido = 0;
+    for (let index in numeros) {
+        let verificaNumero = numeros[index];
+        for (let index2 in numeros) {
+            if (verificaNumero === numeros[index2]) {
+                contNumero += 1;
+            }
+        }
+        if (contNumero > contRepetido) {
+            contRepetido = contNumero;
+            indexNumeroRepetido = index;
+        }
+        contNumero = 0;
+    }
+    return numeros[indexNumeroRepetido];
 }
-return countMaiorNumero;
+console.log(maisRepetido([-4, 15, 1, 7, 1, 9, 0, 1]));
+
 
   // retornar o valor no formato de telefone (12) 34567-8901
 }
 console.log(generatePhoneNumber([-4, 15, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  somaLados = Math.abs(lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB);
+
+  subtraiLados = Math.abs(lineA > lineB - lineC && lineB > lineA - lineC && lineC < lineA > lineB);
+
+  eTriangulo = true;
+
+  if (somaLados == true && subtraiLados == true){
+    eTriangulo = true;
+  } else {
+    eTriangulo = false;
+  }
+console.log(triangleCheck(10, 14, 8))
 }
 
 // Desafio 13
-function hydrate() {
+function hydrate(frase) {
   // seu código aqui
+  let resposta = ' ' + 'copos de água';
+
+  if()
 }
 
 module.exports = {
